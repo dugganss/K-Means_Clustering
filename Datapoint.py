e45ae31d13b37b4a2d019b9f1b4ef2e31e8ca3a6
Sam Duggan <samduggan@me.com>
2025-01-28 16:02:03 +0000

    Complete implementation of k means - correctly calculates silhouette coefficient for all datapoints - uses silhouette coefficient to calculate optimal value of k (if not given or when findOptimalK is called. - comments have been cleaned to describe the new code - adds datapoint class so that data points can store their inter/intra cluster distances

---
+++ Datapoint.py
@@ -0,0 +1,39 @@
+from ClusterEntity import ClusterEntity
+
+'''
+This class represents a Datapoint in the K-Means algorithm
+
+it inherits from ClusterEntity which provides the dimensions and centroid_id attributes
+
+In this context: 
+- dimensions refers to the single row of the DataFrame that a Datapoint can represent.
+- centroid_id refers to the centroid_id of the ClusterEntity that this datapoint is associated with.
+
+New attributes and methods:
+- meanIntraClusterDistance- refers to the average euclidean distance between datapoints with the same centroid_id
+- meanInterClusterDistance- refers to the average euclidean distance between datapoints with the centroid_id 
+closest (by euclidean distance) to the current datapoint's centroid_id.
+- calculateSilhouetteCoefficient() calculates the silhouette coefficient for the current datapoint using the inter/intra 
+cluster distances, using the formula:
+
+    s = (b - a) / max(a, b)
+    
+    where: 
+    - a = meanIntraClusterDistance
+    - b = meanInterClusterDistance
+    - s = Silhouette Coefficient
+    
+(Silhouette Coefficient is used to represent the strength of grouping in the clusters which helps inform the best 
+value of K)
+'''
+
+class Datapoint(ClusterEntity):
+    def __init__(self, dimensions, centroid_id = None):
+        super().__init__(dimensions, centroid_id )
+        self.meanIntraClusterDistance = 0
+        self.meanInterClusterDistance = 0
+        self.silhouetteCoefficient = 0
+
+    def calculateSilhouetteCoefficient(self):
+        self.silhouetteCoefficient =  (self.meanInterClusterDistance - self.meanIntraClusterDistance)/ max(self.meanIntraClusterDistance,self.meanInterClusterDistance)
+        return self.silhouetteCoefficient
